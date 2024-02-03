b8b29efdbe5739a4b9d94459c210a8131132e03a
Jack Davenport <jack.davenport@futuretechaustralia.org>
2024-02-03 23:39:56 +1100

    Add build config for windows/mac executables

---
+++ build-osx.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# NOTE: this script requires jar2app to be installed!
+# get it from: https://github.com/wvdschel/jar2app/tree/master
+# run this script AFTER running './gradlew build'
+
+MAIN_CLASS="com.ft.routing.App"
+JRE_HOME="$(readlink -f "$HOME/.sdkman/candidates/java/current")/*.jdk"
+OUT_DIR="build/osx"
+APP_NAME="FutureTechRouting.app"
+
+mkdir -p $OUT_DIR
+rm -rf $OUT_DIR/*
+
+echo "JRE Home: $JRE_HOME"
+
+jar2app build/libs/ft-*-all.jar $OUT_DIR/$APP_NAME \
+    -n "FutureTech\\ Routing" \
+    -v "1.0.0" \
+    -b "$MAIN_CLASS" \
+    -c "Copyright (c) FutureTech Australia 2024" \
+    -m $MAIN_CLASS \
+    -r $JRE_HOME \  
+    -i icons/mac.icns
--- src/main/java/com/ft/routing/ui/MailboxTableModel.java
+++ src/main/java/com/ft/routing/ui/MailboxTableModel.java
@@ -1,3 +1,9 @@
+/****************************************************************
+* Copyright (c) FutureTech Australia 2024
+* All rights reserved, this code is available for educational
+* purposes. Do not copy or redistribute.
+* @author Jack Davenport
+****************************************************************/
 package com.ft.routing.ui;
 
 import java.util.HashSet;
